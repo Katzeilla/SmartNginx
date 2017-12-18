85dc3a1811371e792560745e2ce8b63ce2c5d5dd
Katzeilla <hello@katzeilla.org>
2017-12-18 18:40:58 -0500

    Add files && update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,22 @@
 #! /bin/bash
 
+cd ./inside
+dir=$(pwd)
+
+if [ $1 == debug ]
+	then
+		flag='bash'
+		echo 'Debug Mode'
+	else
+		echo 'Normal Mode'
+	fi
+
 docker run -it \
   -p 80:80 \
   -p 443:443 \
-  --mount type=bind,source=./inside/configs/,target=/configs/ \
-  --mount type=bind,source=./inside/data/,target=/data/ \
-  --mount type=bind,source=./inside/logs/,target=/logs/ \
-  --mount type=bind,source=./inside/configs/verynginx/,target=/opt/verynginx/verynginx/configs/ \
-  --mount type=bind,source=./inside/configs/nginx/nginx.conf,target=/usr/local/nginx/conf/nginx.conf \
-  nginx:v2 \
-  bash
+  --mount type=bind,source=$dir/configs/,target=/configs/ \
+  --mount type=bind,source=$dir/data/,target=/data/ \
+  --mount type=bind,source=$dir/logs/,target=/logs/ \
+  --mount type=bind,source=$dir/configs/verynginx/config.json,target=/opt/verynginx/verynginx/configs/config.json \
+  --mount type=bind,source=$dir/configs/nginx/nginx.conf,target=/usr/local/nginx/conf/nginx.conf \
+  nginx:v2 $flag
