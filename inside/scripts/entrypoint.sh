8a415bbfd663a17f1b37c3d976537fa48d33f3e8
Katzeilla <hello@katzeilla.org>
2017-12-19 05:45:43 -0500

    Add auto reload config for nginx (every 3 seconds) Auto restart Nginx & Cron Add main loop to keep container running (check status every 60 seconds)

--- inside/scripts/entrypoint.sh
+++ inside/scripts/entrypoint.sh
@@ -8,33 +8,87 @@ start_cron ()
 
   /usr/sbin/cron
 
-  cron_status=$?
+  status=$?
 
-  if [ $cron_status -ne 0 ]; then
-    echo "Failed to start cron: exit $cron_status"
-  exit $cron_status
-fi
+  if [[ $status -ne 0 ]]; then
+      echo "Failed to start cron: exit $status"
+      exit $status
+  fi
 
 }
 
 start_nginx ()
 {
   echo "Starting Nginx......"
-
+  
   /usr/local/nginx/sbin/nginx
+  
+  status=$?
 
-  nginx_status=$?
-
-    if [ $nginx_status -ne 0 ]; then
-      echo "Failed to start Nginx: exit $nginx_status"
-    exit $nginx_status
-  else
-    exit 0;
+  if [[ $status -ne 0 ]]; then
+     echo "Failed to start Nginx: exit $status"
+     exit $status
   fi
 }
 
+start_cron
+start_nginx
 
-  start_cron
-  ps aux
-  start_nginx
-  ps aux
+nginx_restart_count=0
+cron_restart_count=0
+
+# Main loop for every 1 min.
+
+auto_reload()
+{
+    while /bin/true; do
+        /usr/local/nginx/sbin/nginx -s reload
+        sleep 3
+    done 
+}
+
+auto_reload &
+
+while /bin/true; do
+
+    ps aux | grep nginx | grep -q -v grep
+    nginx_status=$?
+    ps aux | grep cron | grep -q -v grep
+    cron_status=$?
+
+    if [[ $cron_status -ne 0 ]]; then
+        let cron_restart_count++
+        echo "cron exited with code $cron_status"
+        
+        # Avoid "4/3 attempt"
+        if [[ $cron_restart_count < "4" ]]; then
+        echo "Attempt to restart, this is the $cron_restart_count/3 attempt"
+        fi
+   fi
+
+    if [[ $nginx_status -ne 0 ]]; then
+        let nginx_restart_count++ 
+        echo "Nginx exited with code $nginx_status"
+        
+        # Avoid "4/3 attempt"
+        if [[ $nginx_restart_count < "4" ]]; then
+        echo "Attempt to restart, this is the $nginx_restart_count/3 attempt"
+        fi
+    fi
+
+
+   # echo $nginx_status
+   # echo $cron_status
+   # echo $nginx_restart_count
+   # echo $cron_status
+   # For debug
+
+
+    if [[ $nginx_restart_count -gt 3 ]] || [[ $cron_restart_count -gt 3 ]]; then
+        echo "To many restart, exit now......"
+        exit 1;
+    fi
+
+    sleep 60
+
+  done
