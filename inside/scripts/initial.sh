5476ee8a06ee8580b32ad3fb7ea3f86d099b42ec
Katzeilla <hello@katzeilla.org>
2017-12-21 01:58:30 -0500

    Optimize output

--- inside/scripts/initial.sh
+++ inside/scripts/initial.sh
@@ -10,22 +10,22 @@ while read i; do
     echo $date "Found domain name" $i
     
   if [ -d /configs/$i ]; then
-    echo $date "Found config directory for" $i ", skip mkdir"
+    echo $date "Found config directory for '$i', skip mkdir"
   else
-    echo $date "Config directory for" $i "not found, create it......"
+    echo $date "Config directory for '$i' not found, create it......"
     mkdir /configs/web/$i
   fi
 
   if [ -f /configs/$i/*.conf ]; then
-    echo $date "Found .conf file for" $i ", skip mkdir"
+    echo $date "Found .conf file for '$i', skip mkdir"
   else
-    echo $date ".conf file for" $i "not found, generate it......"
+    echo $date ".conf file for '$i' not found, generate it......"
   fi
 
   if [ -d /logs/web/$i ]; then
-    echo $date "Log directory for" $i "found, skip mkdir"
+    echo $date "Log directory for '$i' found, skip mkdir"
   else
-    echo $date "Log directory for" $i "not found, create it......"
+    echo $date "Log directory for '$i' not found, create it......"
     mkdir -p /logs/web/$i
   fi
 
