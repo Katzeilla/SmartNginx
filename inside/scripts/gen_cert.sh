fe1363f88210a295542c58d1e803e670f0ce0796
Katzeilla <hello@katzeilla.org>
2017-12-21 18:39:12 -0500

    Update gen_cert.sh

--- inside/scripts/gen_cert.sh
+++ inside/scripts/gen_cert.sh
@@ -1,3 +1,30 @@
 #! /bin/bash
 
+if [[ $2 == gen_initial_conf ]]; then
+  gen_initial_conf
+  gen_cert
+  gen_conf
+  install_cert
+else
+  gen_cert
+  install_cert
+fi
 
+gen_initial_conf()
+{
+
+}
+
+gen_cert()
+{
+
+}
+
+gen_conf()
+{
+
+}
+
+install_cert() {
+
+}
