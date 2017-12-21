a1fee21ee9bbfb59491783c30c5b83570f7b7600
Katzeilla <hello@katzeilla.org>
2017-12-21 02:44:42 -0500

    Auto generate .conf from .template

--- inside/scripts/initial.sh
+++ inside/scripts/initial.sh
@@ -4,22 +4,33 @@ date=[$(date)]
 
 domain_list=/configs/smartnginx/domain_list
 
+# Usage:
+# gen_conf <location of config file> <domain name>
+
+
+gen_conf()
+{
+  cp /configs/nginx/web.conf.template $1
+  sed -i -e "s/<domain_name>/$2/g" $1
+}
+
 echo $date "Reading from" $domain_list
 
 while read i; do
     echo $date "Found domain name" $i
     
-  if [ -d /configs/$i ]; then
+  if [ -d /configs/web/$i ]; then
     echo $date "Found config directory for '$i', skip mkdir"
   else
     echo $date "Config directory for '$i' not found, create it......"
     mkdir /configs/web/$i
   fi
 
-  if [ -f /configs/$i/*.conf ]; then
+  if [ -f /configs/web/$i/*.conf ]; then
     echo $date "Found .conf file for '$i', skip mkdir"
   else
     echo $date ".conf file for '$i' not found, generate it......"
+    gen_conf /configs/web/$i/$i.conf $i
   fi
 
   if [ -d /logs/web/$i ]; then
