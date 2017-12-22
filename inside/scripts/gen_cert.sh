3063a9947b87a5a85d4a0a0116a40663c4323483
Katzeilla <hello@katzeilla.org>
2017-12-21 19:27:12 -0500

    Update gen_cert.sh

--- inside/scripts/gen_cert.sh
+++ inside/scripts/gen_cert.sh
@@ -2,6 +2,8 @@
 
 date=[$(date)]
 
+alias acme.sh=/root/.acme.sh/acme.sh
+
 if [[ $2 == gen_initial_conf ]]; then
   gen_initial_conf
   gen_cert
@@ -14,20 +16,23 @@ fi
 
 gen_initial_conf()
 {
-
+  cp /configs/nginx/web.conf.template.initial /configs/web/$1/$1.conf
+  sed -i -e "s/<domain_name>/$1/g" /configs/web/$1.conf
 }
 
 gen_cert()
 {
-
+  acme.sh 
+  rm /configs/web/$1/$1.conf
 }
 
 gen_conf()
 {
-
+  cp ../configs/nginx/web.conf.template ../configs/web/$1/$1_https.conf
+  sed -i -e "s/<domain_name>/$1/g" ../configs/web/$1/$1_https.conf
 }
 
 install_cert()
 {
-
+  acme.sh 
 }
