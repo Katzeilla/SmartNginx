5efc4bd37529ed4b97c37c31a283595dd5263a58
Katzeilla <hello@katzeilla.org>
2018-01-24 03:10:21 -0500

    Fix typo

--- inside/scripts/preprocess.sh
+++ inside/scripts/preprocess.sh
@@ -2,13 +2,13 @@
 
 date=[$(date)]
 
-maintained_domain_list=/config/smartnginx/maintained_domain_list
+maintained_domain_list=/configs/smartnginx/maintained_domain_list
 
-new_domain_list=/config/smartnginx/new_domain_list
+new_domain_list=/configs/smartnginx/new_domain_list
 
-sorted_domain_list=/config/smartnginx/sorted_domain_list
+sorted_domain_list=/configs/smartnginx/sorted_domain_list
 
-domain_list=/config/smartnginx/domain_list
+domain_list=/configs/smartnginx/domain_list
 
 acme.sh --list | tail -n +2 | cut -f 1 | sort -u > $maintained_domain_list
 
