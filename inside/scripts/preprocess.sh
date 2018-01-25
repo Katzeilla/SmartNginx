d8b45a6a73a6356cdec004b1554460ddcd25aa98
Katzeilla <hello@katzeilla.org>
2018-01-25 04:45:28 -0500

    Debug

--- inside/scripts/preprocess.sh
+++ inside/scripts/preprocess.sh
@@ -10,8 +10,6 @@ sorted_domain_list=/configs/smartnginx/sorted_domain_list
 
 domain_list=/configs/smartnginx/domain_list
 
-~/.acme.sh/acme.sh --list | tail -n +2 | cut -f 1 | sort -u > $maintained_domain_list
-
 sort $domain_list > $sorted_domain_list
 
 comm -13 $maintained_domain_list $sorted_domain_list > $new_domain_list
