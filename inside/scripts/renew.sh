ce99c4acaaa05ade74c8042a7a6f98ac372fbac7
Katzeilla <hello@katzeilla.org>
2018-03-09 20:58:48 -0500

    Update renew.sh

--- inside/scripts/renew.sh
+++ inside/scripts/renew.sh
@@ -6,4 +6,9 @@ domain_list=/configs/smartnginx/domain_list
 
 while read i; do
     echo $date "Found domain name" $i
+
+    echo $date echo [$i] Start renew......
+
+    /scripts/gen_cert.sh $i renew
+
 done < $domain_list
