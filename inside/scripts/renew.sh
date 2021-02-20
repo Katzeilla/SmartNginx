5049d8b8505bdee5b8f5ddd91cefb67d9688d1dc
Katzeilla <hello@katzeilla.org>
2021-02-20 08:08:22 -0500

    renew.sh: disable SC2162 on shellcheck as backslash doesn't matter here

--- inside/scripts/renew.sh
+++ inside/scripts/renew.sh
@@ -1,22 +1,20 @@
 #! /bin/bash
 
-function renew() {
+function renew()
+{
 
 DOMAIN_LIST=/configs/smartnginx/domain_list
 
+# disable SC2162 in shellcheck as backslash doesn't matter here
+# shellcheck disable=SC2162
 while read domain; do
     echo "[$(date)]" "Found domain name" "$domain"
-
     echo "[$(date)]" [$domain] Start renew......
-
     /scripts/gen_cert.sh "$domain" renew
-
 done < $DOMAIN_LIST
 
-    echo "[$(date)]" Reloading Nginx to apply new cert......
-
+echo "[$(date)]" Reloading Nginx to apply new cert......
 /usr/local/nginx/sbin/nginx -s reload
-
 }
 
 renew > /logs/renew/renew-"$(date +%Y-%m-%d-%H%M)".log 2>&1 
