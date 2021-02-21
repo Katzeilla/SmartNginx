1aa2e71a9cfe6d58de2097bc194f2b97c6d14fb1
Katzeilla <hello@katzeilla.org>
2021-02-20 21:51:13 -0500

    gen_cert.sh: linting using shellcheck

--- inside/scripts/gen_cert.sh
+++ inside/scripts/gen_cert.sh
@@ -2,10 +2,10 @@
 
 if [[ -f run_count ]]; then
     # Read run_count from file
-    run_count=$(<run_count)
+    run_count="$(<run_count)"
 else
     echo 0 > run_count
-    run_count=$(<run_count)
+    run_count="$(<run_count)"
 fi
     
 (( run_count++ ))
@@ -15,13 +15,14 @@ echo $run_count > run_count
 
 nginx_signal()
 {
-  /usr/local/nginx/sbin/nginx -s $1
+  /usr/local/nginx/sbin/nginx -s "$1"
 }
 
 gen_initial_conf()
 {
-  cp /configs/nginx/web.conf.template.initial /configs/web/$1/$1.conf
-  sed -i -e "s/<domain_name>/$1/g" /configs/web/$1/$1.conf
+  domain="$1"
+  cp /configs/nginx/web.conf.template.initial "/configs/web/$domain/$domain.conf"
+  sed -i -e "s/<domain_name>/$domain/g" "/configs/web/$domain/$domain.conf"
   /usr/local/nginx/sbin/nginx
 }
 
@@ -40,85 +41,98 @@ gen_cert()
     _arg="--staging"
   fi
 
+    domain="$1"
+
     /root/.acme.sh/acme.sh \
     "$_arg" \
     --issue \
     --force \
-    -d $1 \
-    -w /data/acme.sh/$1/challenges/
+    -d "$domain" \
+    -w /data/acme.sh/"$domain"/challenges/
 
     /root/.acme.sh/acme.sh \
     --install-cert \
     --nocron \
-    -d $1 \
-    --key-file /data/cert/$1/ras/key.pem \
-    --fullchain-file /data/cert/$1/ras/cert.pem 
+    -d "$domain" \
+    --key-file /data/cert/"$domain"/ras/key.pem \
+    --fullchain-file /data/cert/"$domain"/ras/cert.pem 
     
-    if !(is_staging); then
-      echo "[$(date)]" [$1] Submit RAS certificate to COMODO sabre ct log server......
-      ct-submit sabre.ct.comodo.com < /data/cert/$1/ras/cert.pem > /data/cert/$1/ras/sct/comodo_sabre.sct
-      echo "[$(date)]" [$1] Submit RAS certificate to COMODO mammoth ct log server......
-      ct-submit mammoth.ct.comodo.com < /data/cert/$1/ras/cert.pem > /data/cert/$1/ras/sct/comodo_mammoth.sct
+    if ! (is_staging); then
+      echo "[$(date)]" ["$domain"] Submit RAS certificate to COMODO sabre ct log server......
+      ct-submit sabre.ct.comodo.com \
+	      < /data/cert/"$domain"/ras/cert.pem \
+	      > /data/cert/"$domain"/ras/sct/comodo_sabre.sct
+      echo "[$(date)]" ["$domain"] Submit RAS certificate to COMODO mammoth ct log server......
+      ct-submit mammoth.ct.comodo.com \
+	      < /data/cert/"$domain"/ras/cert.pem \
+	      > /data/cert/"$domain"/ras/sct/comodo_mammoth.sct
     fi
 
   /root/.acme.sh/acme.sh \
     "$_arg" \
     --issue \
     --force \
-    -d $1 \
-    -w /data/acme.sh/$1/challenges/ \
+    -d "$domain" \
+    -w /data/acme.sh/"$domain"/challenges/ \
     --keylength ec-384
   
   /root/.acme.sh/acme.sh \
     --install-cert \
-    -d $1 \
+    -d "$domain" \
     --nocron \
     --ecc \
-    --key-file /data/cert/$1/ecc/key.pem \
-    --fullchain-file /data/cert/$1/ecc/cert.pem
+    --key-file /data/cert/"$domain"/ecc/key.pem \
+    --fullchain-file /data/cert/"$domain"/ecc/cert.pem
  
-  if !(is_staging); then
-    echo "[$(date)]" [$1] Submit ECC certificate to COMODO sabre ct log server......
-    ct-submit sabre.ct.comodo.com < /data/cert/$1/ecc/cert.pem > /data/cert/$1/ecc/sct/comodo_sabre.sct
+  if ! (is_staging); then
+    echo "[$(date)]" ["$domain"] Submit ECC certificate to COMODO sabre ct log server......
+    ct-submit sabre.ct.comodo.com \
+	    < /data/cert/"$domain"/ecc/cert.pem \
+	    > /data/cert/"$domain"/ecc/sct/comodo_sabre.sct
     echo "[$(date)]" [$1] Submit ECC certificate to COMODO mammoth ct log server......
-    ct-submit mammoth.ct.comodo.com < /data/cert/$1/ecc/cert.pem > /data/cert/$1/ecc/sct/comodo_sabre.sct
+    ct-submit mammoth.ct.comodo.com \
+	    < /data/cert/"$domain"/ecc/cert.pem \
+	    > /data/cert/"$domain"/ecc/sct/comodo_sabre.sct
   fi
 
-  rm /configs/web/$1/$1.conf
+  rm "/configs/web/$domain/$domain.conf"
 }
 
 gen_main_conf()
 {
-  cp /configs/nginx/web.conf.template.main /configs/web/$1/$1_final.conf
-  sed -i -e "s/<domain_name>/$1/g" /configs/web/$1/$1_final.conf
+  domain="$1"
+  cp /configs/nginx/web.conf.template.main /configs/web/"$domain"/"$domain"_final.conf
+  sed -i -e "s/<domain_name>/$domain/g" /configs/web/"$domain"/"$domain"_final.conf
 
 }
 
 gen_sub_conf()
 {
-  cp /configs/nginx/web.conf.template.sub /configs/web/$1/$1_final.conf
-  sed -i -e "s/<domain_name>/$1/g" /configs/web/$1/$1_final.conf
+  domain="$1"
+  cp /configs/nginx/web.conf.template.sub /configs/web/"$domain"/"$domain"_final.conf
+  sed -i -e "s/<domain_name>/$domain/g" /configs/web/"$domain"/"$domain"_final.conf
 
 }
 
-if [[ $2 == gen_initial_conf ]]; then
+if [[ "$2" == gen_initial_conf ]]; then
 
-    gen_initial_conf $1
-    gen_cert $1
+    domain="$1"
+    gen_initial_conf "$domain"
+    gen_cert "$domain"
 
-        if [[ $run_count == 1 ]]; then
-            gen_main_conf $1
-        else
-            gen_sub_conf $1
-        fi
+    if [[ "$run_count" == 1 ]]; then
+      gen_main_conf "$domain"
+    else
+      gen_sub_conf "$domain"
+    fi
     
     nginx_signal stop
 
-elif [[ $2 == renew ]]; then
-    gen_cert $1
+elif [[ "$2" == renew ]]; then
+    gen_cert "$domain"
 
 else	
-    gen_cert $1
+    gen_cert "$domain"
     stop_nginx
 
 fi
